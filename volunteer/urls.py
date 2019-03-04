7aac9adca046b3a657c2affb906e1878e3a5f221
Nolan-Little <little.nolan@gmail.com>
2019-03-04 16:03:48 -0600

    save unique sign up url, display on dashboard

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^dashboard', views.dashboard, name="dashboard"),
     url(r'^new_event_template', views.new_event_template, name="new_event_template"),
     url(r'^schedule_event', views.schedule_event, name="schedule_event"),
+    url(r'^sign_up/(?P<unique_url>[-\w]+)', views.sign_up, name="sign_up"),
 ]
--- volunteer/views/dashboard_views.py
+++ volunteer/views/dashboard_views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from volunteer.models import Organization
+from volunteer.models import Organization, EventTemplate, ScheduledEvent
 
 def dashboard(request):
     user = request.user
     org = Organization.objects.get(user=user)
+    event_templates = EventTemplate.objects.filter(organization=org)
+    scheduled_events = ScheduledEvent.objects.all()
 
+    for template in event_templates:
+        scheduled_events.filter(event_template=template)
 
     context = {
-        "org": org
+        "org": org,
+        "event_templates": event_templates,
+        "scheduled_events": scheduled_events
     }
 
     return render(request, "dashboard.html", context)
