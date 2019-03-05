58f01d3b5b38c7ffd50d670d75b854059d0dd45d
Nolan-Little <little.nolan@gmail.com>
2019-03-05 14:22:32 -0600

    dynamically create shift fields with unique id's

---
+++ volunteer/static/scripts/addShiftInput.js
@@ -0,0 +1,71 @@
+let shiftInputContainer = document.querySelector('.shift-inputs')
+let fragment = document.createDocumentFragment()
+let addShiftBtn = document.querySelector('#addShiftInput')
+
+
+addShiftBtn.addEventListener('click', (e) => {
+  appendInput(e)
+  shiftInputContainer.appendChild(fragment)
+})
+
+
+function appendInput(e) {
+  current_num_input = numInputGroups(shiftInputContainer)
+  let shiftInputGroup = createInputGroup(current_num_input)
+  fragment.appendChild(shiftInputGroup)
+}
+
+function createInputGroup(current_num_input) {
+  console.log(current_num_input)
+  let inputGroupContainer = document.createElement('div')
+  inputGroupContainer.setAttribute("class", "input--group")
+  inputGroupContainer.setAttribute("id", `inputGroup${current_num_input}`)
+  inputGroupContainer.setAttribute("name", `inputGroup${current_num_input}`)
+
+  let startLabel = document.createElement('label')
+  startLabel.setAttribute("for", `startTime${current_num_input}`)
+  startLabel.textContent = "Shift Start Time"
+  let startTime = document.createElement('input')
+  startTime.setAttribute("type", "time")
+  startTime.setAttribute("id", `startTime${current_num_input}`)
+  startTime.setAttribute("name", `startTime${current_num_input}`)
+
+  let endLabel = document.createElement('label')
+  endLabel.setAttribute("for", `endTime${current_num_input}`)
+  endLabel.textContent = "Shift end Time"
+  let endTime = document.createElement('input')
+  endTime.setAttribute("type", "time")
+  endTime.setAttribute("id", `endTime${current_num_input}`)
+  endTime.setAttribute("name", `endTime${current_num_input}`)
+
+  let descriptionLabel = document.createElement('label')
+  descriptionLabel.setAttribute("for", `description${current_num_input}`)
+  descriptionLabel.textContent = "Shift Description"
+  let description = document.createElement('input')
+  description.setAttribute("type", "textArea")
+  description.setAttribute("id", `description${current_num_input}`)
+  description.setAttribute("name", `description${current_num_input}`)
+
+  let numVolunteersLabel = document.createElement('label')
+  numVolunteersLabel.setAttribute("for", `numVolunteers${current_num_input}`)
+  numVolunteersLabel.textContent = "# Volunteers needed"
+  let numVolunteers = document.createElement('input')
+  numVolunteers.setAttribute("type", "number")
+  numVolunteers.setAttribute("id", `numVolunteers${current_num_input}`)
+  numVolunteers.setAttribute("name", `numVolunteers${current_num_input}`)
+
+  inputGroupContainer.appendChild(startLabel)
+  inputGroupContainer.appendChild(startTime)
+  inputGroupContainer.appendChild(endLabel)
+  inputGroupContainer.appendChild(endTime)
+  inputGroupContainer.appendChild(numVolunteersLabel)
+  inputGroupContainer.appendChild(numVolunteers)
+  inputGroupContainer.appendChild(descriptionLabel)
+  inputGroupContainer.appendChild(description)
+
+  return inputGroupContainer
+}
+
+function numInputGroups(container) {
+  return container.childNodes.length
+}
