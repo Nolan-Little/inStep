ddc5f0e61b74263baee629f26a2163606044e560
Nolan-Little <little.nolan@gmail.com>
2019-03-06 11:33:22 -0600

    individal shift form fields

--- volunteer/static/scripts/addShiftInput.js
+++ volunteer/static/scripts/addShiftInput.js
@@ -8,52 +8,53 @@ addShiftBtn.addEventListener('click', (e) => {
   shiftInputContainer.appendChild(fragment)
 })
 
-
 function appendInput(e) {
-  current_num_input = numInputGroups(shiftInputContainer)
-  let shiftInputGroup = createInputGroup(current_num_input)
+  unique_input_num = uniqueInputId(shiftInputContainer)
+  let shiftInputGroup = createInputGroup(unique_input_num)
   fragment.appendChild(shiftInputGroup)
 }
 
 // TODO: create input and label factory to refactor these next 1000000 lines in to something actually readable
-function createInputGroup(current_num_input) {
-  console.log(current_num_input)
+function createInputGroup(unique_input_num) {
   let inputGroupContainer = document.createElement('div')
-  inputGroupContainer.setAttribute("class", "input--group")
-  inputGroupContainer.setAttribute("id", `inputGroup${current_num_input}`)
-  inputGroupContainer.setAttribute("name", `inputGroup${current_num_input}`)
+  inputGroupContainer.setAttribute("class", "input--group m-2")
+  inputGroupContainer.setAttribute("id", `inputGroup${unique_input_num}`)
+  inputGroupContainer.setAttribute("name", `inputGroup${unique_input_num}`)
+
+  let inputGroupHeader = document.createElement('h5')
+  inputGroupHeader.textContent = `Shift #${unique_input_num}`
 
   let startLabel = document.createElement('label')
-  startLabel.setAttribute("for", `startTime${current_num_input}`)
+  startLabel.setAttribute("for", `startTime${unique_input_num}`)
   startLabel.textContent = "Shift Start Time"
   let startTime = document.createElement('input')
   startTime.setAttribute("type", "time")
-  startTime.setAttribute("id", `startTime${current_num_input}`)
-  startTime.setAttribute("name", `startTime${current_num_input}`)
+  startTime.setAttribute("id", `startTime${unique_input_num}`)
+  startTime.setAttribute("name", `startTime${unique_input_num}`)
 
   let endLabel = document.createElement('label')
-  endLabel.setAttribute("for", `endTime${current_num_input}`)
+  endLabel.setAttribute("for", `endTime${unique_input_num}`)
   endLabel.textContent = "Shift end Time"
   let endTime = document.createElement('input')
   endTime.setAttribute("type", "time")
-  endTime.setAttribute("id", `endTime${current_num_input}`)
-  endTime.setAttribute("name", `endTime${current_num_input}`)
+  endTime.setAttribute("id", `endTime${unique_input_num}`)
+  endTime.setAttribute("name", `endTime${unique_input_num}`)
 
   let descriptionLabel = document.createElement('label')
-  descriptionLabel.setAttribute("for", `description${current_num_input}`)
+  descriptionLabel.setAttribute("for", `description${unique_input_num}`)
   descriptionLabel.textContent = "Shift Description"
   let description = document.createElement('input')
   description.setAttribute("type", "textArea")
-  description.setAttribute("id", `description${current_num_input}`)
-  description.setAttribute("name", `description${current_num_input}`)
+  description.setAttribute("id", `description${unique_input_num}`)
+  description.setAttribute("name", `description${unique_input_num}`)
 
   let numVolunteersLabel = document.createElement('label')
-  numVolunteersLabel.setAttribute("for", `numVolunteers${current_num_input}`)
+  numVolunteersLabel.setAttribute("for", `numVolunteers${unique_input_num}`)
   numVolunteersLabel.textContent = "# Volunteers needed"
   let numVolunteers = document.createElement('input')
   numVolunteers.setAttribute("type", "number")
-  numVolunteers.setAttribute("id", `numVolunteers${current_num_input}`)
-  numVolunteers.setAttribute("name", `numVolunteers${current_num_input}`)
+  numVolunteers.setAttribute("id", `numVolunteers${unique_input_num}`)
+  numVolunteers.setAttribute("name", `numVolunteers${unique_input_num}`)
 
 
   let removeButton = document.createElement('button')
@@ -63,19 +64,21 @@ function createInputGroup(current_num_input) {
     e.target.parentNode.remove()
   })
 
-  inputGroupContainer.appendChild(startLabel)
-  inputGroupContainer.appendChild(startTime)
-  inputGroupContainer.appendChild(endLabel)
-  inputGroupContainer.appendChild(endTime)
-  inputGroupContainer.appendChild(numVolunteersLabel)
-  inputGroupContainer.appendChild(numVolunteers)
-  inputGroupContainer.appendChild(descriptionLabel)
-  inputGroupContainer.appendChild(description)
-  inputGroupContainer.appendChild(removeButton)
+  childArr = [inputGroupHeader, startLabel, startTime, endLabel, endTime, numVolunteersLabel, numVolunteers, descriptionLabel, description, removeButton]
+
+  appendChildren(inputGroupContainer, childArr)
 
   return inputGroupContainer
 }
 
-function numInputGroups(container) {
-  return container.childNodes.length
+// finds the unique Id # of the last line of inputs and returns the iterated num
+function uniqueInputId(container) {
+  if (container.childElementCount === 0) return 1
+  return Number(container.lastChild.id.slice(-1)) + 1
+}
+
+function appendChildren(container, childArr) {
+  for (child of childArr) {
+    container.appendChild(child)
+  }
 }
--- volunteer/views/event_views.py
+++ volunteer/views/event_views.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from volunteer.models import Organization, EventTemplate, ScheduledEvent, ShiftTemplate
-from volunteer.forms import EventTemplateForm
+from volunteer.forms import EventTemplateForm, ShiftTemplateForm
 
 NUM_SHIFT_INPUT_GROUP_VALS = 4
+
+
 def new_event_template(request):
     if request.method == "GET":
         new_event_form = EventTemplateForm()
@@ -22,40 +24,54 @@ def new_event_template(request):
             'location': form_data['location']
         }
 
-        # num 1 accounts for csrf token in POST to properly eval the number of shift input groups
-        num_shift_groups = (len(request.POST) - (len(event_form_data) + 1)) / NUM_SHIFT_INPUT_GROUP_VALS
-
         new_org_form = EventTemplateForm(event_form_data)
         if new_org_form.is_valid():
             org = Organization.objects.filter(user=request.user)[0]
-            event_template = EventTemplate.objects.create(
-                                name=event_form_data['name'],
-                                description=event_form_data['description'],
-                                venue=event_form_data['venue'],
-                                location=event_form_data['location'],
-                                organization=org
-                            )
-            for i in range(int(num_shift_groups)):
-                shift_data = {
-                    'start_time': form_data[f'startTime{i+1}'],
-                    'end_time': form_data[f'endTime{i+1}'],
-                    'num_volunteers': form_data[f'numVolunteers{i+1}'],
-                    'description': form_data[f'description{i+1}'],
-                }
+            new_event_template = EventTemplate.objects.create(
+                name=event_form_data['name'],
+                description=event_form_data['description'],
+                venue=event_form_data['venue'],
+                location=event_form_data['location'],
+                organization=org
+            )
 
-                ShiftTemplate.objects.create(
-                    start_time=shift_data['start_time'],
-                    end_time=shift_data['end_time'],
-                    num_volunteers=shift_data['num_volunteers'],
-                    description=shift_data['description'],
-                    event_template=event_template
-                )
-
-            return HttpResponseRedirect(reverse('volunteer:dashboard'))
+            return HttpResponseRedirect(reverse('volunteer:new_shift_template', args=(new_event_template.id, )))
 
         else:
             template_name = 'events/new_event_template.html'
             return render(request, template_name, {'new_event_form': new_event_form})
+
+
+def new_shift_template(request, event_template_id):
+    event_template = EventTemplate.objects.get(pk=event_template_id)
+
+    if request.method == "GET":
+        template_name = 'events/shift_template.html'
+        shift_form = ShiftTemplateForm()
+        context = {
+            'event_template': event_template,
+            'shift_form': shift_form
+        }
+        return render(request, template_name, context)
+
+    elif request.method == "POST":
+        form_data = request.POST
+        event_form_data = {
+            'start_time': form_data['start_time'],
+            'end_time': form_data['end_time'],
+            'description': form_data['description'],
+            'num_volunteers': form_data['num_volunteers']
+        }
+
+        new_shift_template = ShiftTemplate.objects.create(
+            start_time=event_form_data['start_time'],
+            end_time=event_form_data['end_time'],
+            num_volunteers=event_form_data['num_volunteers'],
+            description=event_form_data['description'],
+            event_template=event_template
+        )
+
+        return HttpResponseRedirect(reverse('volunteer:new_shift_template', args=(event_template.id, )))
 
 
 def schedule_event(request):
