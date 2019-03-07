ed057a3164d82cc5f0a94a45a32eeedfe33b7a39
Nolan-Little <little.nolan@gmail.com>
2019-03-07 16:28:33 -0600

    populate edit shift form

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     url(r'^event_template_details/(?P<event_template_id>\d+)/$', views.event_template_details, name="event_template_details"),
     url(r'^edit_event_template/(?P<event_template_id>\d+)/$', views.edit_event_template, name="edit_event_template"),
     url(r'^new_shift_template/(?P<event_template_id>\d+)/$', views.new_shift_template, name="new_shift_template"),
+    url(r'^edit_shift/(?P<shift_id>\d+)/$', views.edit_shift, name="edit_shift"),
     url(r'^schedule_event', views.schedule_event, name="schedule_event"),
     url(r'^sign_up/(?P<unique_url>[-\w]+)', views.sign_up, name="sign_up"),
 ]
--- volunteer/views/event_views.py
+++ volunteer/views/event_views.py
@@ -86,40 +86,6 @@ def edit_event_template(request, event_template_id):
             return HttpResponseRedirect(reverse('volunteer:dashboard'))
 
 
-def new_shift_template(request, event_template_id):
-    event_template = EventTemplate.objects.get(pk=event_template_id)
-
-    if request.method == "GET":
-        current_shifts = ShiftTemplate.objects.filter(
-            event_template=event_template)
-        template_name = 'events/shift_template.html'
-        shift_form = ShiftTemplateForm()
-        context = {
-            'event_template': event_template,
-            'shift_form': shift_form,
-            'current_shifts': current_shifts
-        }
-        return render(request, template_name, context)
-
-    elif request.method == "POST":
-        form_data = request.POST
-        event_form_data = {
-            'start_time': form_data['start_time'],
-            'end_time': form_data['end_time'],
-            'description': form_data['description'],
-            'num_volunteers': form_data['num_volunteers']
-        }
-
-        new_shift_template = ShiftTemplate.objects.create(
-            start_time=event_form_data['start_time'],
-            end_time=event_form_data['end_time'],
-            num_volunteers=event_form_data['num_volunteers'],
-            description=event_form_data['description'],
-            event_template=event_template
-        )
-
-        return HttpResponseRedirect(reverse('volunteer:new_shift_template', args=(event_template.id, )))
-
 
 def schedule_event(request):
     org = Organization.objects.filter(user=request.user)[0]
