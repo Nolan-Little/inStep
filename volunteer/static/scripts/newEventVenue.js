fd730297da71dd02f79c0f3390f048b1ad56f62b
Nolan-Little <little.nolan@gmail.com>
2019-03-08 15:24:43 -0600

    event form validation

--- volunteer/static/scripts/newEventVenue.js
+++ volunteer/static/scripts/newEventVenue.js
@@ -1,21 +1,31 @@
 let venueSelect = document.querySelector('#id_venue')
-let nameInput = document.querySelector('#id_new_venue_name').parentElement
-let locationInput = document.querySelector('#id_new_venue_location').parentElement
+let nameInput = document.querySelector('#id_new_venue_name')
+let locationInput = document.querySelector('#id_new_venue_location')
 
+if (venueSelect.value == '') toggleNewVenueInputs('show')
+if (venueSelect.value !== '') toggleNewVenueInputs('hide')
 
 venueSelect.addEventListener('change', (e) => {
-  if (e.target.value == '') toggleNewVenueInputs(false)
-  else if (e.target.value !== '') toggleNewVenueInputs(true)
+  if (e.target.value == '') toggleNewVenueInputs('show')
+  else if (e.target.value !== '') toggleNewVenueInputs('hide')
 })
 
-function toggleNewVenueInputs(boolean) {
-  if (boolean){
-    nameInput.setAttribute("hidden", boolean)
-    locationInput.setAttribute("hidden", boolean)
+function toggleNewVenueInputs(state) {
+  if (state === 'hide'){
+    nameInput.removeAttribute("required")
+    locationInput.removeAttribute("required")
+    nameInput.setAttribute("disabled", true)
+    locationInput.setAttribute("disabled", true)
+    nameInput.parentElement.setAttribute("hidden", true)
+    locationInput.parentElement.setAttribute("hidden", true)
   }
-  else if (!boolean){
-    nameInput.removeAttribute("hidden", boolean)
-    locationInput.removeAttribute("hidden", boolean)
+  else if (state === 'show'){
+    nameInput.setAttribute("required", true)
+    locationInput.setAttribute("required", true)
+    nameInput.removeAttribute("disabled")
+    locationInput.removeAttribute("disabled")
+    nameInput.parentElement.removeAttribute("hidden")
+    locationInput.parentElement.removeAttribute("hidden")
   }
 
 }
