4d5d5e1d2d431a18497a53aa9bd94132b8c5aa31
Nolan-Little <little.nolan@gmail.com>
2019-03-11 11:54:29 -0500

    delete event template

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     url(r'^new_event_template', views.new_event_template, name="new_event_template"),
     url(r'^event_template_details/(?P<event_template_id>\d+)/$', views.event_template_details, name="event_template_details"),
     url(r'^edit_event_template/(?P<event_template_id>\d+)/$', views.edit_event_template, name="edit_event_template"),
+    url(r'^delete_event/(?P<event_id>\d+)/$', views.delete_event, name="delete_event"),
     url(r'^new_shift_template/(?P<event_id>\d+)/$', views.new_shift, name="new_shift"),
     url(r'^edit_shift/(?P<shift_id>\d+)/$', views.edit_shift, name="edit_shift"),
     url(r'^delete_shift/(?P<shift_id>\d+)/$', views.delete_shift, name="delete_shift"),
