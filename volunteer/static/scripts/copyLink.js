48ead7153c521036f67c8db32208455e1d9e8f6d
Nolan-Little <little.nolan@gmail.com>
2019-03-11 15:23:30 -0500

    copy sign up link to clipboard

--- volunteer/static/scripts/copyLink.js
+++ volunteer/static/scripts/copyLink.js
@@ -2,5 +2,7 @@ copyBtn = document.querySelector('#copyLink')
 url = document.querySelector('#url')
 
 copyBtn.addEventListener('click', (e) => {
-
+  url.select()
+  document.execCommand("copy")
+  console.log(url.value)
 })
