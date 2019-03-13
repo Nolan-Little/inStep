d936cd90f3e82e19d7c181bab74718b4b16210b6
Nolan-Little <little.nolan@gmail.com>
2019-03-13 11:46:09 -0500

    cal event listeners

--- volunteer/static/scripts/yearCal.js
+++ volunteer/static/scripts/yearCal.js
@@ -1,7 +1,32 @@
-monday = document.querySelectorAll('.mon')
+const mondays = document.querySelectorAll("td.mon")
+const tuesdays = document.querySelectorAll("td.tue")
+const wednesdays = document.querySelectorAll("td.wed")
+const thursdays= document.querySelectorAll("td.thu")
+const fridays = document.querySelectorAll("td.fri")
+const saturdays = document.querySelectorAll("td.sat")
+const sundays = document.querySelectorAll("td.sun")
 
-for (monday of monday) {
-  monday.addEventListener('click', (e) => {
-    console.log(e)
-  })
+allDays = [mondays, tuesdays, wednesdays, thursdays, fridays, saturdays, sundays]
+
+for (dayList of allDays) {
+  daysEventListeners(dayList)
 }
+
+
+function toggleSelected(e) {
+  if (e.target.classList.contains("selected")) {
+    e.target.classList.remove("bg-primary", "text-white", "selected")
+  } else if (!e.target.classList.contains("selected")) {
+    e.target.classList.add("bg-primary", "text-white", "selected")
+  }
+}
+
+function daysEventListeners(dayList) {
+  for (day of dayList) {
+    day.addEventListener("click", (e) => {
+      console.log(e)
+      toggleSelected(e)
+    })
+  }
+}
+
