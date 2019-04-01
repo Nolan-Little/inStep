4785ac741b4749709545b232310d0ab385da5208
Nolan-Little <little.nolan@gmail.com>
2019-04-01 12:03:02 -0500

    copylink bug on mobile safari

--- volunteer/static/scripts/copyLink.js
+++ volunteer/static/scripts/copyLink.js
@@ -4,6 +4,8 @@ copyBtn.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     url = e.target.nextElementSibling
     url.select()
+    url.focus()
+    url.setSelectionRange(0,999)
     document.execCommand("copy")
     url.blur()
   })
